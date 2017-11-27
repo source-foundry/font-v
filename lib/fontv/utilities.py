0ce7ccaa0f48a479bcf2f953afd2b2e05ede96aa
Chris Simpkins <git.simpkins@gmail.com>
2017-11-27 10:41:19 -0500

    refactored git repository root directory search function to utilities module

--- lib/fontv/utilities.py
+++ lib/fontv/utilities.py
@@ -25,3 +25,38 @@ def file_exists(filepath):
         return True
     else:
         return False
+
+
+def get_git_root_path(filepath):
+    """
+    Recursively searches for git root path over 4 directory levels above working directory
+    :param: filepath (string) - path to the font file that is under git version control
+    :return: validated git root path as string
+    :raises: IOError if unable to detect the root of the git repository through this path traversal
+    """
+
+    # begin by defining directory that contains font as the git root needle
+    unverified_gitroot_path = os.path.dirname(os.path.abspath(filepath))
+
+    # check to see if this assumption is correct
+    if dir_exists(os.path.join(unverified_gitroot_path, '.git')):
+        verified_gitroot_path = os.path.join(unverified_gitroot_path, '.git')
+    else:  # if not, recursive search up to three directories above for the git repo root
+        one_level_up = os.path.abspath(os.path.join(unverified_gitroot_path, os.pardir))
+        two_levels_up = os.path.dirname(one_level_up)
+        three_levels_up = os.path.dirname(two_levels_up)
+
+        one_level_up_path = os.path.join(one_level_up, '.git')
+        two_levels_up_path = os.path.join(two_levels_up, '.git')
+        three_levels_up_path = os.path.join(three_levels_up, '.git')
+
+        if dir_exists(one_level_up_path):  # check one directory level up
+            verified_gitroot_path = os.path.dirname(one_level_up_path)
+        elif dir_exists(two_levels_up_path):  # check two directory levels up
+            verified_gitroot_path = os.path.dirname(two_levels_up_path)
+        elif dir_exists(three_levels_up_path):  # check three directory levels up
+            verified_gitroot_path = os.path.dirname(three_levels_up_path)
+        else:
+            raise IOError("Unable to determine git repository root for font file " + filepath)
+
+    return verified_gitroot_path
