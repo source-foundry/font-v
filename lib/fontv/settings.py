d666b0908baa6a6f12cb5742fe483167077320f3
Chris Simpkins <git.simpkins@gmail.com>
2018-01-01 12:21:10 -0500

    updated in application help documentation

--- lib/fontv/settings.py
+++ lib/fontv/settings.py
@@ -19,12 +19,18 @@ patch_version = "0"
 
 HELP = """====================================================
 font-v
-Copyright 2017 Christopher Simpkins
+Copyright 2018 Christopher Simpkins
 MIT License
 Source: https://github.com/source-foundry/font-v
 ====================================================
 
-font-v is a font version string reporting and modification tool for ttf and otf fonts.
+font-v is a font version string reporting and modification tool for ttf and otf fonts.  It is built with the libfv library and supports the OpenFV semantic font versioning specification.
+
+USAGE:
+
+Include a subcommand and desired options in your command line request:
+
+   font-v [subcommand] (options) [font file path 1] ([font file path ...])
 
 Subcommands and options:
 
@@ -32,23 +38,25 @@ Subcommands and options:
     --dev - include all nameID 5 x platformID records in report
     
  write - modify the version string with the following options:
-   --dev - add *dev* build metadata tag (mutually exclusive with --rel)
-   --rel -  add *release* build metadata tag (mutually exclusive with --dev)
-   --sha1 - add git sha1 short hash tag
-   --ver=[version string] - change version number with `1_000` or `1-000` format
+   --dev - add development status metadata (mutually exclusive with --rel)
+   --rel -  add release status metadata (mutually exclusive with --dev)
+   --sha1 - add git commit sha1 short hash state metadata
+   --ver=[version #] - change version number to `version #` definition
 
-Please note:
+NOTES:
 
-The write subcommand --dev and --rel flags are mutually exclusive. 
+The write subcommand --dev and --rel flags are mutually exclusive. Include up to one of these options.
 
-The --ver= definition should replace the desired version period character with an underscore or dash.  This means that 2.001 is defined with either of the following:
+For platforms that treat the period as a special shell character, the --ver=[version #] version number definition should replace the desired version period glyph with an underscore or dash glyph.  For example, 2.001 can be defined with either of the following:
 
    $ font-v write --ver=2_001
    $ font-v write --ver=2-001
    
-You can use more than one option in the same command to combine desired changes to the version string.
+You can include version number, status, and state options in the same request to make all of these modifications simultaneously.
 
-The write subcommand modifies all nameID 5 records identified in the OpenType name table (i.e. across all platformID)
+The write subcommand modifies all nameID 5 records identified in the OpenType name table of the font (i.e. across all platformID).
+
+font-v and the underlying libfv library follow the OpenFV semantic font versioning specification.  This specification can be viewed at https://github.com/openfv/openfv.
 
 """
 
