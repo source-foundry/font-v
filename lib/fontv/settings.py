1a8004d7747d758e508aed3bbbd8d191fdefe797
Chris Simpkins <git.simpkins@gmail.com>
2018-06-14 20:51:37 -0400

    version bump

--- lib/fontv/settings.py
+++ lib/fontv/settings.py
@@ -10,8 +10,8 @@ lib_name = 'font-v'
 # Version Number
 # ------------------------------------------------------------------------------
 major_version = "0"
-minor_version = "6"
-patch_version = "5"
+minor_version = "7"
+patch_version = "0"
 
 # ------------------------------------------------------------------------------
 # Help String
